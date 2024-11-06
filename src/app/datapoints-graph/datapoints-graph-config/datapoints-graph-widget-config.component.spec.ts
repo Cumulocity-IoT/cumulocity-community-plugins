e9f86e66a3d78257c5a0ddc4e5db7817767eff02
Enio Sultan <enio.sultan@cumulocity.com>
2024-11-06 13:21:12 +0100

    Fixed unit tests

--- src/app/datapoints-graph/datapoints-graph-config/datapoints-graph-widget-config.component.spec.ts
+++ src/app/datapoints-graph/datapoints-graph-config/datapoints-graph-widget-config.component.spec.ts
@@ -29,7 +29,10 @@ import { DatapointSelectorModule } from '@c8y/ngx-components/datapoint-selector'
 import { aggregationType } from '@c8y/client';
 import { AnimationBuilder } from '@angular/animations';
 import { take } from 'rxjs/operators';
-import { WidgetConfigComponent } from '@c8y/ngx-components/context-dashboard';
+import {
+  ContextDashboardComponent,
+  WidgetConfigComponent,
+} from '@c8y/ngx-components/context-dashboard';
 import {
   AlarmEventSelectionListComponent,
   AlarmDetails,
@@ -97,6 +100,10 @@ describe('DatapointsGraphWidgetConfigComponent', () => {
         },
         { provide: ChartEventsService, useValue: {} },
         { provide: ChartAlarmsService, useValue: {} },
+        {
+          provide: ContextDashboardComponent,
+          useValue: { isDeviceTypeDashboard: true },
+        },
       ],
     });
     await TestBed.compileComponents();
@@ -114,7 +121,7 @@ describe('DatapointsGraphWidgetConfigComponent', () => {
   });
 
   describe('ngOnInit', () => {
-    it('should set contextAsset for datapoint selector', () => {
+    it('should set contextAsset for datapoint selector when having device type dashboard', () => {
       // when
       component.ngOnInit();
       // then
