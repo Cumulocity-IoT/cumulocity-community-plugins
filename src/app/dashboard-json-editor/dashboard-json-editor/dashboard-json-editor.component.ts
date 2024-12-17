b0d389f5bbea940b523cd39b68a3c057cc7a250e
jdre-c8y <jakub.drewniak@cumulocity.com>
2024-12-17 10:48:40 +0100

    feat(edit-dashboard-json): [MTM-61842] Validate KPI widget

--- src/app/dashboard-json-editor/dashboard-json-editor/dashboard-json-editor.component.ts
+++ src/app/dashboard-json-editor/dashboard-json-editor/dashboard-json-editor.component.ts
@@ -1,5 +1,11 @@
 import { ContextDashboard } from '@c8y/ngx-components/context-dashboard/context-dashboard.model';
-import { Component, inject, OnInit } from '@angular/core';
+import {
+  Component,
+  inject,
+  OnInit,
+  signal,
+  WritableSignal,
+} from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap/modal';
 import {
   CommonModule,
@@ -9,6 +15,16 @@ import {
   ModalModule,
 } from '@c8y/ngx-components';
 import { EditorComponent } from '@c8y/ngx-components/editor';
+import schema from '../kpi_config.json';
+import Ajv from 'ajv';
+
+function replacer(_key: any, value: any) {
+  // Filtering out properties
+  if (value === null) {
+    return undefined;
+  }
+  return value;
+}
 
 @Component({
   selector: 'c8y-dashboard-json-editor',
@@ -27,6 +43,12 @@ import { EditorComponent } from '@c8y/ngx-components/editor';
           monacoEditorMarkerValidator
         ></c8y-editor>
       </div>
+      <button class="btn btn-default" type="button" (click)="validate()">
+        Validate
+      </button>
+      <div *ngIf="errors().length">
+        <p *ngFor="let error of errors()">{{ error }}</p>
+      </div>
     </c8y-modal>
   `,
   standalone: true,
@@ -36,10 +58,13 @@ export class DashboardJsonEditorComponent implements OnInit {
   dashboard!: ContextDashboard;
   valueString = '';
   labels: ModalLabels = { ok: gettext('Save'), cancel: gettext('Cancel') };
+  ajv = new Ajv({ verbose: true, allErrors: true });
 
   result: Promise<string> = new Promise((resolve) => {
     this._close = resolve;
   });
+
+  errors: WritableSignal<string[]> = signal([]);
 
   private _close: ((value: string) => void) | undefined;
   private modalRef = inject(BsModalRef);
@@ -55,5 +80,19 @@ export class DashboardJsonEditorComponent implements OnInit {
 
   onDismiss() {
     this.modalRef.hide();
+  }
+
+  validate() {
+    const dashboard: ContextDashboard = JSON.parse(this.valueString);
+    for (const [_, value] of Object.entries(dashboard.children!)) {
+      const config = value.config;
+      // removing null values as typescript jsom schema doesn't provide info about optional properties
+      const configWithoutNulls = JSON.parse(JSON.stringify(config, replacer));
+
+      const validate = this.ajv.compile(schema);
+
+      validate(configWithoutNulls);
+      this.errors.set(this.ajv.errorsText(validate.errors).split(','));
+    }
   }
 }
