13d768ccbe1fa88bed0374a83196d1f78278fce5
jdre-c8y <jakub.drewniak@cumulocity.com>
2024-12-17 11:18:21 +0100

    feat(edit-dashboard-json): [MTM-61842] Validate dashboard

--- src/app/dashboard-json-editor/dashboard-json-editor/dashboard-json-editor.component.ts
+++ src/app/dashboard-json-editor/dashboard-json-editor/dashboard-json-editor.component.ts
@@ -15,7 +15,8 @@ import {
   ModalModule,
 } from '@c8y/ngx-components';
 import { EditorComponent } from '@c8y/ngx-components/editor';
-import schema from '../kpi_config.json';
+import kpiSchema from '../kpi_config.json';
+import dashboardSchema from '../dashboard_config.json';
 import Ajv from 'ajv';
 
 function replacer(_key: any, value: any) {
@@ -25,6 +26,11 @@ function replacer(_key: any, value: any) {
   }
   return value;
 }
+
+export const Schemas = {
+  dashboard: 'dashboard',
+  kpiWidget: 'kpiWidget',
+} as const;
 
 @Component({
   selector: 'c8y-dashboard-json-editor',
@@ -36,7 +42,7 @@ function replacer(_key: any, value: any) {
       (onClose)="onSave()"
       (onDismiss)="onDismiss()"
     >
-      <div [ngStyle]="{ height: '500px' }" class="d-flex">
+      <div [ngStyle]="{ height: '350px' }" class="d-flex">
         <c8y-editor
           class="flex-grow"
           [(ngModel)]="valueString"
@@ -46,8 +52,13 @@ function replacer(_key: any, value: any) {
       <button class="btn btn-default" type="button" (click)="validate()">
         Validate
       </button>
-      <div *ngIf="errors().length">
-        <p *ngFor="let error of errors()">{{ error }}</p>
+      <div *ngIf="dashboardErrors().length">
+        <strong>Dashboard errors:</strong>
+        <p *ngFor="let error of dashboardErrors()">{{ error }}</p>
+      </div>
+      <div *ngIf="widgetErrors().length">
+        <strong>Widget errors:</strong>
+        <p *ngFor="let error of widgetErrors()">{{ error }}</p>
       </div>
     </c8y-modal>
   `,
@@ -58,19 +69,25 @@ export class DashboardJsonEditorComponent implements OnInit {
   dashboard!: ContextDashboard;
   valueString = '';
   labels: ModalLabels = { ok: gettext('Save'), cancel: gettext('Cancel') };
+
+  // TODO: ajv should be singleton (recommendation from docs), so separate service necessary
   ajv = new Ajv({ verbose: true, allErrors: true });
 
   result: Promise<string> = new Promise((resolve) => {
     this._close = resolve;
   });
 
-  errors: WritableSignal<string[]> = signal([]);
+  dashboardErrors: WritableSignal<string[]> = signal([]);
+  widgetErrors: WritableSignal<string[]> = signal([]);
 
   private _close: ((value: string) => void) | undefined;
   private modalRef = inject(BsModalRef);
 
   ngOnInit(): void {
     this.valueString = JSON.stringify(this.dashboard);
+
+    this.ajv.addSchema(kpiSchema, Schemas.kpiWidget);
+    this.ajv.addSchema(dashboardSchema, Schemas.dashboard);
   }
 
   onSave() {
@@ -84,15 +101,31 @@ export class DashboardJsonEditorComponent implements OnInit {
 
   validate() {
     const dashboard: ContextDashboard = JSON.parse(this.valueString);
+    this.validateDashboard(dashboard);
+    this.validateWidgets(dashboard);
+  }
+
+  private validateDashboard(dashboard: ContextDashboard) {
+    const validate = this.ajv.getSchema(Schemas.dashboard);
+    // removing null values as typescript jsom schema doesn't provide info about optional properties
+    const dashboardWithoutNulls = JSON.parse(
+      JSON.stringify(dashboard, replacer)
+    );
+
+    validate?.(dashboardWithoutNulls);
+    this.dashboardErrors.set(this.ajv.errorsText(validate?.errors).split(','));
+  }
+
+  private validateWidgets(dashboard: ContextDashboard) {
     for (const [_, value] of Object.entries(dashboard.children!)) {
       const config = value.config;
       // removing null values as typescript jsom schema doesn't provide info about optional properties
       const configWithoutNulls = JSON.parse(JSON.stringify(config, replacer));
 
-      const validate = this.ajv.compile(schema);
+      const validate = this.ajv.getSchema(Schemas.kpiWidget);
 
-      validate(configWithoutNulls);
-      this.errors.set(this.ajv.errorsText(validate.errors).split(','));
+      validate?.(configWithoutNulls);
+      this.widgetErrors.set(this.ajv.errorsText(validate?.errors).split(','));
     }
   }
 }
