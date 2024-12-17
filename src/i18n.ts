73c083dfa6a3e2e80753127a1a8d809df73b282d
Paweł Rynarzewski <pawel.rynarzewski@cumulocity.com>
2024-12-17 15:32:17 +0100

    feat: [MTM-62062] Add translation workflow
    Signed-off-by: Paweł Rynarzewski <pawel.rynarzewski@cumulocity.com>

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,4 +3,13 @@
  * You can always add additional strings by adding your own po file. All po files are
  * combined to one JSON file per language and are loaded if the specific language is needed.
  */
-import './locales/de.po'; // <- adding additional strings to the german translation.
+import './locales/de.po';
+// import './locales/es.po';
+// import './locales/fr.po';
+// import './locales/ja_JP.po';
+// import './locales/ko.po';
+// import './locales/nl.po';
+// import './locales/pl.po';
+// import './locales/pt_BR.po';
+// import './locales/zh_CN.po';
+// import './locales/zh_TW.po';
