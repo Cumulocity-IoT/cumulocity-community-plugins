9911cf1c12735e792279ef22c4afe86655e209e9
Dawid Janusz <dawid.janusz@cumulocity.com>
2025-09-01 19:56:57 +0200

    fix: correct bootstrap process
      - Replace AppComponent wrapper with direct BootstrapComponent usage
      - Add missing provideBootstrapMetadata for essential Cumulocity services
      - Update app.config.ts providers to use proper Cumulocity pattern
      - Remove non-existent DatapointsGraphWidgetModule from config
      - Fix core bootstrapping issues preventing proper app initialization

--- cumulocity.config.ts
+++ cumulocity.config.ts
@@ -14,12 +14,7 @@ export default {
     contentSecurityPolicy:
       "base-uri 'none'; default-src 'self' 'unsafe-inline' http: https: ws: wss:; connect-src 'self' http: https: ws: wss:;  script-src 'self' *.bugherd.com *.twitter.com *.twimg.com *.aptrinsic.com 'unsafe-inline' 'unsafe-eval' data:; style-src * 'unsafe-inline' blob:; img-src * data: blob:; font-src * data:; frame-src *; worker-src 'self' blob:;",
     dynamicOptionsUrl: '/apps/public/public-options/options.json',
-    remotes: {
-      'sag-pkg-community-plugins': [
-        'ExampleWidgetPluginModule',
-        'DatapointsGraphWidgetModule',
-      ],
-    },
+    remotes: { 'sag-pkg-community-plugins': ['ExampleWidgetPluginModule'] },
     package: 'plugin',
     isPackage: true,
     noAppSwitcher: true,
@@ -64,22 +59,10 @@ export default {
       '@ngx-translate/core',
     ],
     copy: [
-      {
-        from: 'CHANGELOG.md',
-        to: 'CHANGELOG.md',
-      },
-      {
-        from: 'screenshots',
-        to: 'screenshots',
-      },
-      {
-        from: 'c8y-widget-preview-img',
-        to: 'c8y-widget-preview-img',
-      },
-      {
-        from: 'LICENSE',
-        to: 'LICENSE.txt',
-      },
+      { from: 'CHANGELOG.md', to: 'CHANGELOG.md' },
+      { from: 'screenshots', to: 'screenshots' },
+      { from: 'c8y-widget-preview-img', to: 'c8y-widget-preview-img' },
+      { from: 'LICENSE', to: 'LICENSE.txt' },
     ],
   },
 } as const satisfies EnvironmentOptions;
